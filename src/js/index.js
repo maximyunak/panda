80749e6501e749e5b9ef1ad98b5c7e8fefd74848
Maxik-web <kleok0039@gmail.com>
2024-07-23 18:14:24 +0300

    add

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,50 @@
-import "../scss/index/index.scss";
-import "../scss/app/app.scss";
+import '../scss/index/index.scss';
+import '../scss/app/app.scss';
+
+import Swiper from 'swiper/bundle';
+import 'swiper/css';
+// import 'swiper/css/navigation';
+// import 'swiper/css/pagination';
+
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  centeredSlides: true,
+  slidesPerView: 1,
+  spaceBetween: 10,
+  breakpoints: {
+    840: {
+      slidesPerView: 2,
+    },
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  on: {
+    slideChangeTransitionEnd: function () {
+      const slides = document.querySelectorAll('.swiper-slide');
+      slides.forEach((slide) => slide.classList.add('lowered'));
+      const activeSlide = document.querySelector('.swiper-slide-active');
+      activeSlide.classList.remove('lowered');
+    },
+  },
+});
+
+// Initial lowering of side slides
+window.addEventListener('load', () => {
+  const slides = document.querySelectorAll('.swiper-slide');
+  slides.forEach((slide) => slide.classList.add('lowered'));
+  const activeSlide = document.querySelector('.swiper-slide-active');
+  activeSlide.classList.remove('lowered');
+});
+
+const swiperTitle = new Swiper('.title__slider', {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
--- src/js/profile.js
+++ src/js/profile.js
@@ -1,2 +1,18 @@
-import "../scss/app/app.scss";
-import "../scss/profile/profile.scss";
+import '../scss/app/app.scss';
+import '../scss/profile/profile.scss';
+
+import Swiper from 'swiper/bundle';
+import 'swiper/css';
+
+const swiperTitle = new Swiper('.cards__swiper', {
+  // Optional parameters
+  loop: true,
+
+  slidesPerView: 2,
+  spaceBetween: 70,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
