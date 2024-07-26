09d929fead891b82ca914a139bb56504e51526d6
Maxik-web <kleok0039@gmail.com>
2024-07-26 17:35:00 +0300

    fix

--- src/js/index.js
+++ src/js/index.js
@@ -6,13 +6,49 @@ import 'swiper/css';
 // import 'swiper/css/navigation';
 // import 'swiper/css/pagination';
 
+const profile = document.getElementById('profile');
+const referals = document.getElementById('referals');
+const main = document.getElementById('main');
+
+const profilePage = document.querySelector('.profile');
+const referalsPage = document.querySelector('.referals');
+const mainPage = document.querySelector('.main');
+
+profile.addEventListener('click', () => {
+  profilePage.classList.remove('none');
+  mainPage.classList.add('none');
+  referalsPage.classList.add('none');
+
+  profile.classList.add('active');
+  main.classList.remove('active');
+  referals.classList.remove('active');
+});
+referals.addEventListener('click', () => {
+  referalsPage.classList.remove('none');
+  mainPage.classList.add('none');
+  profilePage.classList.add('none');
+
+  referals.classList.add('active');
+  main.classList.remove('active');
+  profile.classList.remove('active');
+});
+main.addEventListener('click', () => {
+  mainPage.classList.remove('none');
+  profilePage.classList.add('none');
+  referalsPage.classList.add('none');
+
+  main.classList.add('active');
+  profile.classList.remove('active');
+  referals.classList.remove('active');
+});
+
 const swiper = new Swiper('.swiper', {
   loop: true,
   centeredSlides: true,
   slidesPerView: 1,
   spaceBetween: 10,
   breakpoints: {
-    840: {
+    450: {
       slidesPerView: 2,
     },
   },
